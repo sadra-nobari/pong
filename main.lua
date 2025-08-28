cbfb8d3fa536270b668865bfe7692aade7dfc134
sadra nobari <sadra.nobari83@gmail.com>
2025-08-28 19:23:58 +0200

    setting fonts and background color - creating paddles and ball

--- main.lua
+++ main.lua
@@ -1,15 +1,56 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
 
 function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    larg_font = love.graphics.newFont('Monocraft.ttf', 32)
+    small_font = love.graphics.newFont('Monocraft.ttf', 8)
+    
+    player1Score = 0
+    player2Score = 0
+
+    love.window.setTitle('Pong')
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         resizable = false,
         vsync = true,
         fullscreen = false
     })
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        highdpi = true,
+        canvas = true
+    })
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
 
 function love.draw()
-    love.graphics.printf("This is Pong!", 0, WINDOW_HEIGHT/2, WINDOW_WIDTH, "center")
+    push:start()
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
+    love.graphics.setFont(larg_font)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT/2 - 80)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT/2 - 80)
+
+    --paddle1
+    love.graphics.rectangle('fill', 10, 10, 5, 20)
+
+    --paddle2
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 30, 5, 20)
+
+    --ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH/2 - 2, VIRTUAL_HEIGHT/2 - 2, 4, 4)
+
+
+    push:finish()
 end
